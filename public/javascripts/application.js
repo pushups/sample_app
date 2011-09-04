2ed3a7091b91e2d21da323a5b1b255fe3765ab24
Michael Williams <merwanwilliams@gmail.com>
2011-09-04 15:10:07 -0700

    Added jquery and character counter

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function count_characters(form) {
+	var characters = form.value.length;
+	var remaining = 140 - characters;
+	console.log(remaining);
+	var message = remaining + ' ' + (remaining == 1 ? 'character' : 'characters') + ' ' + 'remaining.';
+	console.log(message);
+	$('#character_count').html('<p>' + message + '</p>');	
+}
